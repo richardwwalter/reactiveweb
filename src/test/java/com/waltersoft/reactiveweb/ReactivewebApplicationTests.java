8b5264b76f8d443f9de332e3d5db41ca707e5383
richardwwalter <mailstorichard@gmail.com>
2021-03-24 16:23:57 +0000

    Use test containers postgres image

--- src/test/java/com/waltersoft/reactiveweb/ReactivewebApplicationTests.java
+++ src/test/java/com/waltersoft/reactiveweb/ReactivewebApplicationTests.java
@@ -2,8 +2,10 @@ package com.waltersoft.reactiveweb;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@Testcontainers
 class ReactivewebApplicationTests {
 
 	@Test
